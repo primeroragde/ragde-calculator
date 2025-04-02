62dfd086874d8f239cefc2c3773a68cf41b41039
primeroragde <primeroragde@gmail.com>
2025-04-02 14:06:12 -0400

    good progess, needs to work on custom_exponent

---
+++ ConsoleApplication1/calcuator.cpp
@@ -0,0 +1,35 @@
+class Calcuator {
+public:
+
+	double addition(double num1, double num2)
+	{
+		return num1 + num2;
+	};
+
+	double subtraction(double num1, double num2) {
+		return num1 - num2;
+	};
+	
+	double division(double num1, double num2) {
+		return num1 / num2;
+	}
+
+	double multiplication(double num1, double num2)
+	{
+		return num1 * num2;
+	}
+
+	double exponent(double num) // Assumes that default exponent is x^2
+	{
+		return num * num;
+	}
+	
+	double custom_exponent(double num, double numExponent) // maybe convert numExp
+	{
+		for (int i = 0; i < numExponent; i++) // is this slow method? idk might check this later
+		{
+			//TODO: Find how decimal point exponent works
+			
+		}
+	}
+};
---
+++ ConsoleApplication1/calcuator.h
@@ -0,0 +1,14 @@
+class Calcuator {
+public:
+	double userInput;
+
+	double addition(double num1, double num2);
+
+	double subtraction(double num1, double num2);
+
+	double division(double num1, double num2);
+
+	double multiplication(double num1, double num2);
+
+	double custom_exponent(double num, double numExponent) {};
+};
